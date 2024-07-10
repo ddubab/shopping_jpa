a927f78a3d3bf44162df99cff961919592ba68b3
sosososososooo <160336473+ddubab@users.noreply.github.com>
2024-07-10 17:22:00 +0900

    Add files via upload

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() {
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertEquals(findMember.getId(), savedId);
+        Assertions.assertEquals(member, findMember);
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+    }
+
+}
